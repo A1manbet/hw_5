e409b0528ee23466d33324ec0d46407c3183908a
A1manbet <wu34389@gmail.com>
2023-09-16 23:37:17 +0600

    2

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,28 @@ public class Main {
         System.out.println("Damage: " + adamSmasher.getDamage());
         System.out.println("Defence: " + adamSmasher.getDefence());
 
+        Hero[] heroes = createHeroes();
 
+
+        for (Hero x : heroes) {
+            System.out.println("Damage: " + x.getDamage() + " Health: " + x.getHealth() + " Super power: " + x.getSuperPower());
+        }
+
+
+    }
+    public static Hero[] createHeroes(){
+
+        Hero leonardo = new Hero(250, 30, "magical");
+        Hero donatello = new Hero(300, 40);
+        Hero mikey = new Hero(250, 40, "super Fast");
+
+        Hero[] arrayHeroes = new Hero[3];
+
+        arrayHeroes[0] = leonardo;
+        arrayHeroes[1] = donatello;
+        arrayHeroes[2] = mikey;
+
+        return arrayHeroes;
     }
 
 
